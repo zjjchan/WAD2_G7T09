f45c1820bdeeaedb683fd41f8b607bbaf4baf073
zjjchan <justus.chan.2023@scis.smu.edu.sg>
2024-10-16 12:27:14 +0800

    added navbar and homepage.vue file

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { RouterLink, useRoute } from 'vue-router';
+import profilepic from '@/assets/images/profilepic.png'
+
+defineProps({
+    user: String
+});
+
+const isActiveLink = (routepath) => {
+    const route = useRoute();
+    return route.path === routepath;
+}
+
+</script>
+
+<template>
+    <nav class="main-navbar navbar sticky-top navbar-expand-lg navbar-light">
+        <div class="container-fluid col-11">
+            <!-- Navbar brand -->
+            <a class="lily-script-one-regular navbar-brand text-light" href="#">
+                Mealmate
+            </a>
+            <!-- Navbar toggle button -->
+            <button class="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="offcanvas" 
+                    data-bs-target="#sidebar"
+                aria-controls="sidebar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Top menu in navbar -->
+            <div class="collapse navbar-collapse" 
+                id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" 
+                        aria-current="page" href="#">
+                            Recipes
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">
+                            Grocery List
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">
+                            Nutrition
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class=" pfp col-1 d-flex justify-content-center">
+            <img :src="profilepic">
+        </div>
+    </nav>
+
+    <!-- Sidebar Setup -->
+    <div class="main-sidebar offcanvas offcanvas-start" 
+         tabindex="-1" id="sidebar">
+        <div class="offcanvas-header">
+            <button type="button" 
+            class="btn-close text-reset" 
+                    data-bs-dismiss="offcanvas" 
+                    aria-label="Close">
+            </button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">
+                        Recipes
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">
+                        Grocery List
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">
+                        Nutrition
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
