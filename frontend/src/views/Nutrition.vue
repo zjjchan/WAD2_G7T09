a67e8d980d585cd279dfa4309b3240f3ad22d7e0
hansellohs <hansel.loh.2023@scis.smu.edu.sg>
2024-10-20 23:55:39 +0800

    plans on what to inlcude in the nutrition page

--- frontend/src/views/Nutrition.vue
+++ frontend/src/views/Nutrition.vue
@@ -3,6 +3,17 @@
     <div class="container-fluid">
         Nutrition
     </div>
+
+    <div>
+        Show Days of the Week (Mon-Sun)
+    </div>
+    <div>
+        Show Meals eaten each day
+    </div>
+    <div>
+        Show Charts/Visuals of each meal eaten every day of the week
+    </div>
+
 </template>
 
 <script setup>
