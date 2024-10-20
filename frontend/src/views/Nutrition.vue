8ef1302e84d7efeaae4ffcf96ab2bc2aac470b70
hansellohs <hansel.loh.2023@scis.smu.edu.sg>
2024-10-20 23:59:42 +0800

    testing to see how the weekly meals will look like

--- frontend/src/views/Nutrition.vue
+++ frontend/src/views/Nutrition.vue
@@ -7,9 +7,72 @@
     <div>
         Show Days of the Week (Mon-Sun)
     </div>
+
     <div>
         Show Meals eaten each day
     </div>
+
+    <div>
+        <table>
+            <tr>
+                <th>Day</th>
+                <th>Breakfast</th>
+                <th>Lunch</th>
+                <th>Dinner</th>
+                <th>Supper</th>
+            </tr>
+            <tr>
+                <td>Monday</td>
+                <td>Oatmeal</td>
+                <td>Grilled Chicken Salad</td>
+                <td>Spaghetti</td>
+                <td>Fruit Smoothie</td>
+            </tr>
+            <tr>
+                <td>Tuesday</td>
+                <td>Pancakes</td>
+                <td>Turkey Sandwich</td>
+                <td>Stir-fry Vegetables</td>
+                <td>Nuts & Seeds</td>
+            </tr>
+            <tr>
+                <td>Wednesday</td>
+                <td>Scrambled Eggs</td>
+                <td>Chicken Wrap</td>
+                <td>Grilled Salmon</td>
+                <td>Yogurt</td>
+            </tr>
+            <tr>
+                <td>Thursday</td>
+                <td>Smoothie Bowl</td>
+                <td>Pasta Salad</td>
+                <td>Beef Tacos</td>
+                <td>Protein Bar</td>
+            </tr>
+            <tr>
+                <td>Friday</td>
+                <td>Avocado Toast</td>
+                <td>Quinoa Bowl</td>
+                <td>Pizza</td>
+                <td>Dark Chocolate</td>
+            </tr>
+            <tr>
+                <td>Saturday</td>
+                <td>French Toast</td>
+                <td>Caesar Salad</td>
+                <td>Burgers</td>
+                <td>Ice Cream</td>
+            </tr>
+            <tr>
+                <td>Sunday</td>
+                <td>Bagels</td>
+                <td>Fish Tacos</td>
+                <td>Roast Chicken</td>
+                <td>Cheese & Crackers</td>
+            </tr>
+        </table>
+    </div>
+
     <div>
         Show Charts/Visuals of each meal eaten every day of the week
     </div>
