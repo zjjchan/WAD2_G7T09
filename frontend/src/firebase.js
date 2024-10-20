ddee725eb7378e410bb2c111aea724c35de75490
Dwight Chong Jia Hao <dwightchong.2023@scis.smu.edu.sg>
2024-10-20 11:58:38 +0800

    updated main and firebase js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -10,11 +12,10 @@ const firebaseConfig = {
     appId: "1:342958265365:web:9fb8d8d1794a3ccb7c6a1c",
     measurementId: "G-9ZFXVK0JHG"
   };
-  
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
-const db = getFirestore(app);
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 
-export { db };
+export { firebaseApp, analytics, auth, db };
