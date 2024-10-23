41106f1cc4fc0cc05cd1efed8e3c542024c712a2
hansellohs <hansel.loh.2023@scis.smu.edu.sg>
2024-10-23 21:50:07 +0800

    testing pulling dummy data from database

--- frontend/src/views/Nutrition.vue
+++ frontend/src/views/Nutrition.vue
@@ -1,5 +1,3 @@
-
-
 <template>
     <Navbar />
     <div class="container-fluid">
@@ -10,17 +8,43 @@
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import { db } from '../firebase'; // Import your Firebase configuration
-import { collection, getDocs } from 'firebase/firestore';
-
-const querySnapshot = await getDocs(collection(db, 'mealsfortheweek'));
-console.log(querySnapshot);
-
 </script>
 
+<script>
+import { db } from '../firebase';
+// import { firebaseApp } from '../firebase'; // Import your Firebase configuration
+// import { analytics } from '../firebase';
+// import { auth } from '../firebase';
+
+import { doc, collection, getDocs } from 'firebase/firestore';
+
+const fetchData = async () => {
+    try {
+        const querySnapshot = await getDocs(collection(db, 'mealsfortheweek'));
+
+        for (const days of querySnapshot.docs) {
+            console.log(days.id);
+
+            // Fetch meals (subcollection) for each day
+            const mondayDocRef = doc(db, 'mealsfortheweek', 'monday');
+
+            
+            
+        }
+
+    } catch (error) {
+        console.error("Error fetching documents: ", error);
+    }
+};
+
+// Call the fetchData function to retrieve the data
+fetchData();
+</script>
 
 <style>
-table, th, td {
+table,
+th,
+td {
     border: 1px black solid;
 }
 </style>
