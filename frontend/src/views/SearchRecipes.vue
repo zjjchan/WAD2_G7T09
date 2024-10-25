456a88b48dec7adbb4f8af0203855d870ab7669c
yiting-chai <yiting.chai.2023@smu.edu.sg>
2024-10-25 21:10:48 +0800

    added filter function to search recipe

--- frontend/src/views/SearchRecipes.vue
+++ frontend/src/views/SearchRecipes.vue
@@ -1,49 +1,83 @@
 <template>
-    <Navbar />
-    <div class="container-fluid">
-      
-    </div>
-    <div>
-    <!-- search -->
-    
-    <div class="container" id="searchbar">
-      <img id="search_img" src="../assets/images/search.png" alt="search">
-      <input 
-      v-model="query" 
-      placeholder="Search recipe" 
-      @keyup.enter="handleSearch" 
-    />
-      <button @click="handleSearch" class="btn btn-primary">Search</button>
-    </div>
-    <br><br>
+  <Navbar />
+  <div class="container-fluid">
 
-    <!-- Display results -->
-    <div class="container-fluid" v-for="(recipe, index) in filteredRecipes" :key="index">
-      <div class="card col-5">
-        <h5 class="card-title">{{ recipe.label }}</h5>
-        <img id="recipe_img" :src="recipe.image" alt="Recipe Image" />
-        <div class="card-body">
-          <p><strong>Calories Count:</strong> {{ recipe.calories.toFixed(0) }} kcals</p>
-          <p><strong>Health Labels:</strong> {{ recipe.healthLabels.join(', ') }}</p>
-          <p><strong>Diet Labels:</strong> {{ recipe.dietLabels.join(', ') }}</p>
-          <p><strong>Cuisine Type:</strong> {{ recipe.cuisineType.join(', ') }}</p>
-          
-          <RouterLink
-            :to="{ name: 'recipe', params: { label: recipe.label } }"
-            class="btn btn-primary"
-            @click.native="saveSelectedRecipe(recipe)" >
-          
-            More Info
-          </RouterLink>
+    <div class="row">
+
+      <!-- Sidebar for filters -->
+      <div class="col-3">
+        <h5>Filters</h5>
+        <div class="filter-section">
+          <h6>Diet Labels</h6>
+          <div v-for="label in dietLabels" :key="label">
+            <input type="checkbox" :value="label" v-model="selectedDietLabels" />
+            <label>{{ label }}</label>
+          </div>
+        </div>
+
+        <div class="filter-section">
+          <h6>Health Labels</h6>
+          <div v-for="label in healthLabels" :key="label">
+            <input type="checkbox" :value="label" v-model="selectedHealthLabels" />
+            <label>{{ label }}</label>
+          </div>
+        </div>
+
+        <div class="filter-section">
+          <h6>Cuisine Type</h6>
+          <div v-for="cuisine in cuisineTypes" :key="cuisine">
+            <input type="checkbox" :value="cuisine" v-model="selectedCuisineTypes" />
+            <label>{{ cuisine }}</label>
+          </div>
+        </div>
+
+        <button @click="applyFilters" class="btn btn-primary mt-3">Apply Filters</button>
+      </div>
+
+
+      <!-- search -->
+      <div class="col-9">
+        <div>
+          <div class="container" id="searchbar">
+            <img id="search_img" src="../assets/images/search.png" alt="search">
+            <input v-model="query" placeholder="Search recipe" @keyup.enter="handleSearch" />
+            <button @click="handleSearch" class="btn btn-primary">Search</button>
+          </div>
+
+          <br><br>
+
+
+
+          <!-- Display results -->
+          <div v-for="(recipe, index) in filteredRecipes" :key="index" class="container-fluid card mb-3">
+
+            <h5 class="card-title">{{ recipe.label }}</h5>
+            <img id="recipe_img" :src="recipe.image" alt="Recipe Image" />
+            <div class="card-body">
+              <p><strong>Calories Count:</strong> {{ recipe.calories.toFixed(0) }} kcals</p>
+              <p><strong>Health Labels:</strong> {{ recipe.healthLabels.join(', ') }}</p>
+              <p><strong>Diet Labels:</strong> {{ recipe.dietLabels.join(', ') }}</p>
+              <p><strong>Cuisine Type:</strong> {{ recipe.cuisineType.join(', ') }}</p>
+
+              <RouterLink :to="{ name: 'recipe', params: { label: recipe.label } }" class="btn btn-primary"
+                @click.native="saveSelectedRecipe(recipe)">
+
+                More Info
+              </RouterLink>
+
+            </div>
+          </div>
+
+
+          <button @click="loadMore" v-if="recipes.length > 0">Load More</button>
+          <!-- Error message -->
+          <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
         </div>
       </div>
     </div>
 
-    <button @click="loadMore">Load More</button>
-
-    <!-- Error message -->
-    <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
   </div>
+
 </template>
 
 <script setup>
@@ -54,7 +88,7 @@ import { RouterLink, useRoute } from 'vue-router';
 const route = useRoute();
 
 const isActiveLink = (routePath) => {
-    return route.path === routePath;
+  return route.path === routePath;
 }
 </script>
 <script>
@@ -68,13 +102,27 @@ export default {
       apiKey: '160b560497690476362bc1fca361165a', // Replace with your actual API key
       appId: '374ab5b2', // Replace with your actual App ID
       from: 0, // Pagination start index
-      to: 20 // Number of results to fetch
+      to: 20, // Number of results to fetch
+
+      // Filter data
+      dietLabels: ['Balanced', 'High-Protein', 'Low-Carb', 'Low-Fat'],
+      healthLabels: ['Vegan', 'Vegetarian', 'Gluten-Free', 'Dairy-Free'],
+      cuisineTypes: ['Italian', 'Chinese', 'Mexican', 'Indian'],
+
+      // Selected filters
+      selectedDietLabels: [],
+      selectedHealthLabels: [],
+      selectedCuisineTypes: []
     };
   },
   computed: {
     filteredRecipes() {
-      // Show filtered recipes when available
-      return this.recipes;
+      return this.recipes.filter(recipe => {
+        const matchesDiet = !this.selectedDietLabels.length || this.selectedDietLabels.some(label => recipe.dietLabels.includes(label));
+        const matchesHealth = !this.selectedHealthLabels.length || this.selectedHealthLabels.some(label => recipe.healthLabels.includes(label));
+        const matchesCuisine = !this.selectedCuisineTypes.length || this.selectedCuisineTypes.some(cuisine => recipe.cuisineType.includes(cuisine));
+        return matchesDiet && matchesHealth && matchesCuisine;
+      });
     }
   },
   methods: {
@@ -115,6 +163,10 @@ export default {
     loadMore() {
       this.from += this.to; // Increment the starting index for pagination
       this.getData(this.query); // Fetch more data
+    },
+    applyFilters() {
+      // Trigger re-evaluation of filteredRecipes
+      this.filteredRecipes;
     }
   }
 };
@@ -122,10 +174,11 @@ export default {
 </script>
 
 <style scoped>
-#search_img{
+#search_img {
   width: 2rem;
   padding-right: 10px;
 }
+
 #recipe_img {
   width: 200px;
   height: 200px;
@@ -134,20 +187,26 @@ export default {
 .card {
   background-color: #8fb17f;
 }
-#searchbar{
-  border: solid 1px;
+
+#searchbar {
+  border: solid 2px;
   border-radius: 30px;
   margin: auto;
   width: 400px;
   height: 50px;
-  margin-top:50px ;
+  margin-top: 50px;
   align-content: center;
 
 }
-input{
+
+input {
   border: none;
   margin: auto;
-  
+
+}
+
+
+button {
+  margin-left: 30px;
 }
 </style>
-
