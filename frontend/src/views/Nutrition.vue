69e23ac5dcdd675fb97e60b1a75bf4f86ec92b66
hansellohs <hansel.loh.2023@scis.smu.edu.sg>
2024-11-05 00:38:58 +0800

    edits

--- frontend/src/views/Nutrition.vue
+++ frontend/src/views/Nutrition.vue
@@ -4,23 +4,27 @@
         <MealPlanCard />
     </div>
     <div class="row m-2">
-        <div class="col-lg-6 col-md-12 p-2">
+        <div class="col-lg-6 col-md-12 p-1">
             <div class="m-2 h-100">
-                <div id="barcharttext" class="p-1 my-2 border rounded">
+                <div id="barcharttext" class="p-1 border rounded">
                     <h4 id="" class="text-center pt-2">Calorie Consumption for the Week</h4>
                 </div>
-                <div class="p-3" style="">
-                    <NutritionOverview />
+                <div>
+                    <div class="p-3 chartbg">
+                        <NutritionOverview />
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 col-md-12 p-2">
+        <div class="col-lg-6 col-md-12 p-1">
             <div class="m-2 h-100">
-                <div id="piecharttext" class="p-1 my-2 border rounded">
+                <div id="piecharttext" class="p-1 border rounded">
                     <h4 class="text-center pt-2">Calories Consumed each Day of the Week</h4>
                 </div>
-                <div id="piechart" class="p-3">
-                    <NutritionPie />
+                <div>
+                    <div id="piechart" class="p-3 chartbg">
+                        <NutritionPie />
+                    </div>
                 </div>
             </div>
         </div>
@@ -44,11 +48,15 @@ import NutritionPie from "@/components/NutritionPie.vue";
 
 
 <style>
-    #barcharttext, #piecharttext {
-        background-color: rgba(0, 135, 0, 0.5);
-    }
+#barcharttext,
+#piecharttext {
+    background-color: rgba(0, 135, 0, 0.5);
+}
 
-    /* #barchart, #piechart {
+/* .chartbg {
+        background-color: lightgray;
+    } */
+/* #barchart, #piechart {
         background-image: linear-gradient(gray, lightgray);
     }; */
 </style>
