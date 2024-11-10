f951b5234ed00381c5ca03759674d0508408ff63
hansellohs <hansel.loh.2023@scis.smu.edu.sg>
2024-11-11 01:14:42 +0800

    fixed responsiveness for nutrition

--- frontend/src/views/Nutrition.vue
+++ frontend/src/views/Nutrition.vue
@@ -3,8 +3,8 @@
 
     <!-- CHARTS -->
     <div class="row m-1">
-        <div class="col-lg-1 col-md-1 col-1"></div>
-        <div class="col-lg-5 col-md-10 p-1">
+        <div class="col-lg-1 col-md-1 d-none d-lg-block"></div>
+        <div class="col-lg-5 col-md-10 p-1 mx-auto">
             <div class="m-1 h-100">
                 <div id="piecharttext2" class="p-1">
                     <h4 class="text-center pt-2">Calories Consumed (Days of the Week)</h4>
@@ -16,10 +16,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-5 col-md-10 p-1">
+        <div class="col-lg-5 col-md-10 p-1 mx-auto">
             <div class="m-1 h-100">
                 <div id="barcharttext2" class="p-1">
-                    <h4 id="" class="text-center pt-2">Calorie Consumed (Meals & Days of the Week)</h4>
+                    <h4 class="text-center pt-2">Calorie Consumed (Meals & Days of the Week)</h4>
                 </div>
                 <div>
                     <div class="mt-2 p-4 chartbg chart-container">
@@ -28,43 +28,25 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-1 col-md-1 col-1"></div>
+        <div class="col-lg-1 col-md-1 d-none d-lg-block"></div>
     </div>
 
-    <div class="row">
-        <div class="col-1"></div>
-        <div class="col-10 m-4 p-3 ">
+    <!-- MEAL PLAN CARD -->
+    <div class="row justify-content-center">
+        <div class="col-10 col-md-10 col-lg-10 m-4 p-3 mx-auto">
             <MealPlanCard />
         </div>
-        <div class="col-1"></div>
     </div>
-
-
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import MealPlanCard from "@/components/Meal Preferences Components/MealPlanCard.vue";
-import NutritionOverview from "@/components/NutritionOverview.vue";
 import NEWNutritionOverview from "@/components/NEWNutritionOverview.vue";
 import NEWPolarAreaChart from "@/components/NEWPolarAreaChart.vue";
 </script>
 
-<script>
-import { db } from '../firebase';
-import { doc, collection, getDocs, getDoc } from 'firebase/firestore';
-import NutritionPie from "@/components/NutritionPie.vue";
-</script>
-
-
 <style>
-/* #barcharttext,
-#piecharttext {
-    background-color: rgba(0, 135, 0, 0.5);
-} */
-
-
-
 .chart-container {
     overflow: hidden;
     height: 500px;
@@ -73,11 +55,4 @@ import NutritionPie from "@/components/NutritionPie.vue";
 h4 {
     color: rgba(0, 90, 0, 1);
 }
-
-/* .chartbg {
-        background-color: lightgray;
-    } */
-/* #barchart, #piechart {
-        background-image: linear-gradient(gray, lightgray);
-    }; */
 </style>
