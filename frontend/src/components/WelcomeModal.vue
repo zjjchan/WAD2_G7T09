2ca944f04ee99900be681a32b8c19e44b9db65eb
yiting-chai <yiting.chai.2023@smu.edu.sg>
2024-11-13 02:20:02 +0800

    removed 'asian' option

--- frontend/src/components/WelcomeModal.vue
+++ frontend/src/components/WelcomeModal.vue
@@ -147,7 +147,7 @@ const healthLabels = [
 ];
 
 const cuisineTypes = [
-  'American', 'Asian', 'British', 'Caribbean', 'Central Europe',
+  'American', 'British', 'Caribbean', 'Central Europe',
   'Chinese', 'Eastern Europe', 'French', 'Indian', 'Italian',
   'Japanese', 'Kosher', 'Mediterranean', 'Mexican', 'Middle Eastern',
   'Nordic', 'South American', 'South East Asian'
